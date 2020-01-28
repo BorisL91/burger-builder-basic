1c6f3ebc7e47b672a252153e2968adabbfcd424c
Boris Lukanovic <boris.lukanovic17@gmail.com>
2020-01-28 13:40:51 +0100

    minor stuff

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React from "react"
 
-import Aux from "../../hoc/aux";
-import classes from "./Layout.css";
+import Aux from "../../hoc/aux"
+import classes from "./Layout.css"
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.mainContent}>{props.children}</main>
   </Aux>
-);
+)
 
-export default layout;
+export default layout
